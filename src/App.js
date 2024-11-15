37c88111f9a333c8b1bd43b8fae968278602d883
yousef-abdo-taha-10 <yousefabdotaha@gmail.com>
2024-11-15 15:34:52 +0200

    start broject

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import NavBar from './components/Navbar/NavBar';
+import Home from './Home';
+import Aboute from './Aboute';
+import Contact from './Contact';
+function App() {
+  return (
+    <div className="App">
+      
+     
+     <BrowserRouter>
+     <NavBar/>
+     <Routes>
+      <Route path='/Home' element={<Home/>}/>
+      <Route path='/Aboute' element={<Aboute/>}/>
+      <Route path='/Contact' element={<Contact/>}/>
+     </Routes>
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
