89dc32619dd663ba438d3087fb537ff7f7eb2a4b
moawed mohamed <moawedmohamed144@gmail.com>
2024-11-27 20:44:57 +0200

    this is the last commit yet

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import NavBar from './components/Navbar/NavBar';
-import Home from './Home';
-import Aboute from './Aboute';
-import Contact from './Contact';
-<<<<<<< HEAD
-import Login from './Login';
-=======
-import Login from './login';
-
->>>>>>> 4373f4d10bf716005a7f7cd014cff56c9f1e9c99
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Home";
+import Aboute from "./Aboute";
+import Contact from "./Contact";
+import Login from "./Login";
+import DoubleRowNavbar from "./components/Navbar/DoubleRowNavbar";
+import Occordion from "./Accordion";
+import Sliddder from "./Sliddder"
+import Rating from "./Rating"
 function App() {
   return (
     <div className="App">
-      <h1>hhh</h1>
-     <BrowserRouter>
-<<<<<<< HEAD
-    
-=======
-     
->>>>>>> 4373f4d10bf716005a7f7cd014cff56c9f1e9c99
-     <NavBar/>
-     <Routes>
-      <Route path='/Home' element={<Home/>}/>
-      <Route path='/Aboute' element={<Aboute/>}/>
-      <Route path='/Contact' element={<Contact/>}/>
-     </Routes>
-      
-     </BrowserRouter>
-<<<<<<< HEAD
-     
-     
-=======
-     <Login />
->>>>>>> 4373f4d10bf716005a7f7cd014cff56c9f1e9c99
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Aboute" element={<Aboute />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+      <DoubleRowNavbar />
+      <Sliddder />
+      <Occordion />
+      <Rating />
+      <Login />
     </div>
   );
 }
